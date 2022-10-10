072780657b3c6770869ab6c3d6900ee5fd55e4bb
Christopher Mohr <christopher.mohr@uni-tuebingen.de>
2022-10-10 19:42:52 +0200

    check todos

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -9,12 +9,11 @@ class WorkflowMain {
     //
     public static String citation(workflow) {
         return "If you use ${workflow.manifest.name} for your analysis please cite:\n\n" +
-            // TODO nf-core: Add Zenodo DOI for pipeline after first release
-            //"* The pipeline\n" +
-            //"  https://doi.org/10.5281/zenodo.XXXXXXX\n\n" +
-            "* The nf-core framework\n" +
-            "  https://doi.org/10.1038/s41587-020-0439-x\n\n" +
-            "* Software dependencies\n" +
+            '* The pipeline\n' +
+            '  https://doi.org/10.5281/zenodo.1401039-1073c8\n\n' +
+            '* The nf-core framework\n' +
+            '  https://doi.org/10.1038/s41587-020-0439-x\n\n' +
+            '* Software dependencies\n' +
             "  https://github.com/${workflow.manifest.name}/blob/master/CITATIONS.md"
     }
 
@@ -90,4 +89,5 @@ class WorkflowMain {
         }
         return null
     }
+
 }
